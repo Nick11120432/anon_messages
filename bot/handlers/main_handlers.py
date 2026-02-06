4ea0e990f5665695290e39b2e0701dc60cabf226
Nick <nicksavin401@gmail.com>
2026-02-06 18:06:38 +0300

    fix main_handlers.py

--- bot/handlers/main_handlers.py
+++ bot/handlers/main_handlers.py
@@ -11,7 +11,7 @@ import keyboards as keyboards
 import database.requests as requests
 import texts as t
 
-ADMIN_CHAT_ID = config.admin_chat_id
+ADMIN_CHAT_ID = config.get("admin_chat_id")
 
 
 main_router = Router()
